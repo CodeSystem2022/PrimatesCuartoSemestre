f51d25ec9c477456fa4b65aaf3e4e95bcf031b24
tania <tannita2116@gmail.com>
2023-08-31 11:58:13 -0300

    pruebas de usuarioDAO

---
+++ Python/Clase 01/-.py
@@ -0,0 +1,29 @@
+# Profundizando en los sistemas de numeracion
+
+# Sistema decimal
+a = 10
+print(f'a decimal: {a}')
+
+# Sistema binario
+a = 0b1010
+print(f'a binario: {a}')
+
+# Sistema octal
+a = 0o12
+print(f'a octal: {a}')
+
+# Ssitema hexadecimal
+a = 0xA
+print(f'a hexadecimal: {a}')
+
+# Base decimal
+a = int('23', 10)
+print(f'a: {a}')
+
+# Base Binario
+a = int('1010', 2)
+print(f'a binario: {a}')
+
+#Base Octal
+a = int('12')
+
