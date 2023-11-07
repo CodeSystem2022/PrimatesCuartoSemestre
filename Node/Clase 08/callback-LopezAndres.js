9c845f30d9078f79f930e2d4c7ad196f52d13c97
Andres Lopez <111933179+AAndres02@users.noreply.github.com>
2023-11-07 01:04:33 -0300

    Update callback-LopezAndres.js

---
+++ Node/Clase 08/callback-LopezAndres.js
@@ -0,0 +1,12 @@
+function soyAsincrona(miCallback){
+    setTimeout(function(){
+        console.log('Hola, soy una función asíncrona');
+        miCallback();
+    }, 1000);
+
+}
+
+console.log('Iniciando el proceso...');
+soyAsincrona(function(){
+    console.log('Terminando el proceso...')
+})
