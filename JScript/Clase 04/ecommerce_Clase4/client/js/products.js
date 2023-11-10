f837a09c16257fac07b7bd86288242926c1aaa2a
Lucas Rinaldi <111933209+LucaasRin@users.noreply.github.com>
2023-11-10 17:24:18 -0300

    ecommerce_Clase4

---
+++ JScript/Clase 04/ecommerce_Clase4/client/js/products.js
@@ -0,0 +1,30 @@
+const productos = [
+    {
+        id:1,
+        productName: "banana",
+        price: 300,
+        quanty: 1,
+        img: "/client/media/banana.png",
+    },
+    {
+        id: 2,
+        productName: "caramelo",
+        price: 2.500,
+        quanty: 1,
+        img: "/client/media/caramelo.png",
+    },
+    {
+        id: 3,
+        productName: "fideos",
+        price: 150,
+        quanty: 1,
+        img: "/client/media/fideos.png",
+    },
+    {
+        id: 4,
+        productName: "detergente",
+        price: 3600,
+        quanty: 1,
+        img: "/client/media/detergente.png",
+    },
+];
