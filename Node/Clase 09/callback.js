f1a893a0eb4989c42b00dd16d5614939fef4d171
Lucas Rinaldi <111933209+LucaasRin@users.noreply.github.com>
2023-11-10 17:32:02 -0300

    callblack_Clase9

---
+++ Node/Clase 09/callback.js
@@ -0,0 +1,23 @@
+function hola(nombre, miCallback){
+    setTimeout( function() {
+        console.log('Hola ' + nombre);
+        miCallback(nombre);
+    }, 1000);
+}
+
+//Función asincrona - adios
+function adios(nombre, otrocallback) {
+    setTimeout(function(){
+        console.log('Adiós '+ nombre);
+        otrocallback();
+    }, 1500);   
+}
+
+console.log('Iniciando el proceo...');
+
+hola('Lucas', function(nombre) {
+    adios(nombre, function(){
+        console.log('Terminando el proceso...');    
+    });
+});
+
